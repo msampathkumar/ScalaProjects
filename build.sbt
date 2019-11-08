0f8056653507f7d87682c138467b53487c19b768
Sampath Kumar <sam1990kumar@gmail.com>
2019-11-08 18:53:01 +0530

    draft save

--- build.sbt
+++ build.sbt
@@ -5,7 +5,10 @@ version := "0.1"
 scalaVersion := "2.12.0"
 
 libraryDependencies ++= Seq (
-  "org.apache.spark" %% "spark-core" % "2.4.3"
+  "org.apache.spark" %% "spark-core" % "2.4.3",
+  "org.apache.spark" %% "spark-sql" % "2.4.3",
+  "org.apache.spark" %% "spark-graphx" % "2.4.3",
+  "org.apache.spark" %% "spark-mllib" % "2.4.3" % "runtime"
 )
 
 /*
--- src/main/scala/Demo3/Demo3.scala
+++ src/main/scala/Demo3/Demo3.scala
@@ -2,18 +2,22 @@ package Demo3
 
 import org.apache.spark._
 import org.apache.spark.SparkContext._
+import org.apache.log4j._
 
 object Demo3 {
 
   def main(args: Array[String]): Unit = {
     // arguments
+    // TODO: move below file to a local repo
     val input_file_name = "/Users/sampathm/udemy/data/genome-scores.csv"
     val outputFile = "/tmp/genome.out"
 
     // Spark Context
+    Logger.getLogger("org").setLevel(Level.ERROR)
     val conf = new SparkConf().setAppName("wordCount").setMaster("local[*]")
     // Create a Scala Spark Context.
     val sc: SparkContext = new SparkContext(conf)
+    // val sc: SparkContext = new SparkContext("local[*]", "WordCount")
     // Load our input data.
     val input =  sc.textFile(input_file_name)
     // Split up into words.
